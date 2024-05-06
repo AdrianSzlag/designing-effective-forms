e3b3cf041e74251f1015a9b64f17712ca62acf88
Adrian Szlag <92638498+AdrianSzlag@users.noreply.github.com>
2024-05-06 23:02:40 +0000

    Done

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ async function fetchAndFillCountries() {
             throw new Error('Błąd pobierania danych');
         }
         const data = await response.json();
-        const countries = data.map(country => country.name.common);
-        countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+        const countries = data.map(country => country.name.common).sort((a, b) => a.localeCompare(b));
+        countryInput.innerHTML += countries.map(country => `<option value="${country}">${country}</option>`).join('');
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
@@ -30,6 +30,9 @@ function getCountryByIP() {
         .then(data => {
             const country = data.country;
             // TODO inject country to form and call getCountryCode(country) function
+            const countryInput = document.getElementById('country');
+            countryInput.value = country;
+            getCountryCode(country);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -40,25 +43,75 @@ function getCountryCode(countryName) {
     const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
     fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
-        }
-        return response.json();
-    })
-    .then(data => {        
-        const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
-    })
-    .catch(error => {
-        console.error('Wystąpił błąd:', error);
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Błąd pobierania danych');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const countryCodeInput = document.getElementById('countryCode');
+            countryCodeInput.value = data[0].name.common;
+        })
+        .catch(error => {
+            console.error('Wystąpił błąd:', error);
+        });
 }
 
+countryInput.addEventListener('change', (event) => {
+    const countryName = event.target.value;
+    getCountryCode(countryName);
+});
 
-(() => {
+async function injectCountriesPhoneCodes() {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const data = await response.json();
+        console.log(data);
+
+        const countries = data.sort((a, b) => {
+            return a.name.common.localeCompare(b.name.common);
+        }).
+            map(country => {
+                const countryCode = `${country.idd.root}` + `${country.idd.suffixes?.length === 1
+                    ? country.idd.suffixes[0]
+                    : ''
+                    }`;
+
+                return `<option value="${country.name.common}">${country.name.common} (${countryCode})</option>`;
+            });
+
+        const countryCodesInput = document.getElementById('countryCode');
+        countryCodesInput.innerHTML += countries.join('');
+    } catch (error) {
+        console.error('Wystąpił błąd:', error);
+    }
+}
+
+const name = document.getElementById('firstName');
+const surname = document.getElementById('lastName');
+const email = document.getElementById('exampleInputEmail1');
+const invoiceData = document.getElementById('invoiceData'); // textArea
+
+let isInvoicedDataFilled = false;
+
+// when user clicks onto invoice data for the first time, the form will be filled with the same data as in the personal data
+invoiceData.addEventListener('focus', () => {
+    if (!isInvoicedDataFilled) {
+        invoiceData.value = `Imię: ${name.value}\nNazwisko: ${surname.value}\nEmail: ${email.value}`;
+        isInvoicedDataFilled = true;
+    }
+});
+
+
+(async () => {
     // nasłuchiwania na zdarzenie kliknięcia myszką
     document.addEventListener('click', handleClick);
 
+    await injectCountriesPhoneCodes();
     fetchAndFillCountries();
+    getCountryByIP();
 })()
+
+
+
